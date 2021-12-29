6be3533e7be34f364d28504234e67a4bccbc071e
ArhostCode <e-gulyakin@mail.ru>
2021-12-29 21:02:44 +0300

    Cleaning the code

--- src/com/arhostcode/world/Position.java
+++ src/com/arhostcode/world/Position.java
@@ -12,14 +12,6 @@ public class Position implements Cloneable{
         this.y = y;
     }
 
-    public void setY(int y) {
-        this.y = y;
-    }
-
-    public void setX(int x) {
-        this.x = x;
-    }
-
     public int getY() {
         return y;
     }
